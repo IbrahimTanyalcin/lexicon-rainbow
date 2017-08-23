d17981cbb56069fec513fe71bff0c22954a39703
ibrahim <ibrahim.tanyalcin@i-pv.org>
2017-08-23 12:53:39 +0200

    changeTrigger

--- phantomTest.js
+++ phantomTest.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.viewportSize = {width: 1000,height: 360};
+page.viewportSize = {width: 1000,height: 500};
 page.open('./index_v3.html', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
