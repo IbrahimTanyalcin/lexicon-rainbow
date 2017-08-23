e1ff1b4c8768750407dbed3f73d59e07dc84b320
ibrahim <ibrahim.tanyalcin@i-pv.org>
2017-08-23 16:26:17 +0200

    testObject

---
+++ tests/phantomjsTest_or.js
@@ -0,0 +1,58 @@
+var page = require('webpage').create();
+page.viewportSize = {width: 1000,height: 500};
+var runner = new taskRunner;
+taskRunner
+.queue('./tests/index_v3.html')
+.queue('./tests/index_v4.html')
+.run();
+
+page.open('./tests/index_v3.html', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+    setTimeout(function(){
+		page.render('./test.png');
+		var base64 = page.renderBase64("PNG");
+		console.log(base64);
+		console.log("Test png rendered, exiting phantom..");
+		phantom.exit();
+	},120000)
+  }
+});
+
+function taskRunner () {
+	var that = this;
+	var testCount = -1;
+	this.tasks = [];
+	Object.defineProperty(this,
+		"fetch",
+		{
+			get: function(){
+				return this.tasks.pop();
+			},
+			enumerable:true,
+			configurable:true,
+			writable:false
+		}
+	)
+	this.queue = function(q){this.tasks.push(q); testCount++; return this;};
+	this.run = function(){
+		page.open(that.fetch, function(status) {
+			console.log("Status: " + status);
+			if(status === "success") {
+				setTimeout(function(){
+					page.render('./test.'+testCount+'.png');
+					var base64 = page.renderBase64("PNG");
+					console.log(base64);
+					console.log("Test rendered successfully..");
+					testCount--;
+					if (that.tasks.length) {
+						that.run();
+					} else {
+						console.log("Last test was run. Exiting");
+						phantom.exit();
+					}
+				},120000)
+			}
+		});
+	}
+}
