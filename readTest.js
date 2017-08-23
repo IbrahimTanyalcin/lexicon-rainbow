99a7b1ca9d1a4aa7dd304076873d0cd0bfcae95f
ibrahim <ibrahim.tanyalcin@i-pv.org>
2017-08-23 09:41:48 +0200

    changeTrigger

--- readTest.js
+++ readTest.js
@@ -3,5 +3,6 @@ fs.readFile("./test.png","utf8",function(err,res){
 	if (err) {
 		console.log("cannot read file");
 	} 
-	console.log(res);
+	var base64 = new Buffer(res,"binary").toString("base64");
+	console.log(base64);
 });
