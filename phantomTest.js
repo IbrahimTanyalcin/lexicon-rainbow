4c1ae09aeddd8fcf99c787943a07975b1e88dc2f
ibrahim <ibrahim.tanyalcin@i-pv.org>
2017-08-23 10:03:05 +0200

    changeTrigger

--- phantomTest.js
+++ phantomTest.js
@@ -1,9 +1,12 @@
 var page = require('webpage').create();
+page.viewportSize = {width: 1920,height: 1080};
 page.open('./index_v3.html', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
     page.render('./test.png');
-	console.log("Test png rendered, exiting phantom..")
+	var base64 = page.renderBase64("PNG");
+	console.log(base64);
+	console.log("Test png rendered, exiting phantom..");
   }
   phantom.exit();
 });
