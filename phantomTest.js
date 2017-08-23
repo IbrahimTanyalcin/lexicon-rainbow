9907f68bc38cdcd1c94bc927eed698229ce6fd2a
ibrahim <ibrahim.tanyalcin@i-pv.org>
2017-08-23 10:45:59 +0200

    addForTrigger

--- phantomTest.js
+++ phantomTest.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.viewportSize = {width: 800,height: 1600};
+page.viewportSize = {width: 1520,height: 338};
 page.open('./index_v3.html', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
